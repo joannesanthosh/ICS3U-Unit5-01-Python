99733f5289b0c3baa106386d8805556606dbf16d
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-11-25 15:46:36 +0000

    update

---
+++ temperature_program.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Joanne Santhosh
+# Created on: Nov 2022
+# This program converts temperature from celsius to fahrenheit
+
+
+def main():
+    # This function adds up two numbers
+
+    # input
+    temperature_input = int(input("Enter the temperature in celsius:"))
+
+    # process
+    answer = first_number + second_number
+
+    # output
+    print("")
+    print("{0} + {1} = {2}.".format(first_number, second_number, answer))
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
