736ee72df9269ea20180aa8fe2669c57e1b77580
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-11-30 16:01:40 +0000

    update

--- temperature_program.py
+++ temperature_program.py
@@ -9,7 +9,7 @@ def fahrenheit():
     try:
         temp_celsius = int(input("Enter the degrees in celsius: "))
         temp_fahrenheit = (9 / 5) * temp_celsius + 32
-        print("\nThe temperature is {0} fahrenheit.".format(temp_fahrenheit))
+        print("\nThe temperature is {0}° fahrenheit.".format(temp_fahrenheit))
     except Exception:
         print("\nThis is an invalid input. Please try again")
     finally:
